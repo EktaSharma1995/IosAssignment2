dcd9344acc322f28567b48a0f6841365f12e8c20
Ekta Sharma <ektasharma@Ektas-MacBook-Air.local>
2018-09-29 22:55:08 -0400

    Second commit

--- Contents.swift
+++ Contents.swift
@@ -38,6 +38,8 @@ class Client: Bank
     var age: Int
     var dob: String
     var gender: String
+    var account: Account;
+    var pin: Int = 0;
     
     override init()
     {
@@ -46,16 +48,19 @@ class Client: Bank
         age = 0;
         dob = "";
         gender = "";
+        account = Account();
+        pin = 1234;
         super.init();
     }
     
-    init(name: String, address: String, age: Int, dob: String, gender: String, nameOfBank: String)
+    init(name: String, address: String, age: Int, dob: String, gender: String, nameOfBank: String, account: Account)
     {
         self.name = name;
         self.address = address;
         self.age = age;
         self.dob = dob;
         self.gender = gender;
+        self.account = account;
         super.init(nameOfBank: nameOfBank);
     }
     
@@ -64,33 +69,47 @@ class Client: Bank
         
     }
     
-    func changeProfile(newName: String, newAddress: String)
+    func changeName(newName: String)
     {
         self.name = newName;
-        self.address = newAddress;
-        print("Updated name: \(name), Address: \(address)");
+        print("Updated name: \(name)");
     }
+    
+    func changeAddress(newAddress: String)
+    {
+        self.address = newAddress;
+        print("Updated address: \(address)");
+    }
+    
+    func changePin(newPin: Int)
+    {
+        self.pin = newPin;
+        print("Updated pin: \(pin)");
+    }
+    
+    
     
 }
 
 
-class Account: Bank
+class Account
 {
     var type: String;
     var accountNumber = arc4random()
     var balance: Double = 0;
     var initialBalance:Double = 0;
+    var overdraftProtector: Bool ;
     
-    override init()
+    init()
     {
         type = "";
-        super.init();
+        overdraftProtector = false;
     }
     
-    init(type: String, nameOfBank: String, balance: Double)
+    init(type: String, balance: Double, overdraftProtector: Bool)
     {
         self.type = type;
-        super.init(nameOfBank: nameOfBank);
+        self.overdraftProtector = overdraftProtector;
     }
     
     enum accountTypes{
@@ -98,10 +117,11 @@ class Account: Bank
         case Saving
     }
     
-    func createAccount(accountType: accountTypes)
+    var caseAccount = accountTypes.Chequing
+    
+    func createAccount(accountType: accountTypes) -> Bool
     {
-        var caseAccount = accountTypes.Chequing
-        
+        var overdraftStatus = overdraftProtector;
         switch caseAccount
         {
         case .Chequing:
@@ -109,20 +129,70 @@ class Account: Bank
             print("Hey! Your chequing account is created:")
             print("Account# is CHE\(accountNumber)");
             print("Initial Balance:\(initialBalance)")
+            overdraftStatus = false;
         case .Saving:
             initialBalance = 200;
             print("Hey! Your saving account is created:")
             print("Account# is SAV\(accountNumber)");
             print("Initial Balance:\(initialBalance)")
+            overdraftStatus = true;
         default:
             initialBalance = 50;
             print("Hey! Your personal account is created:")
             print("Account# is SAV\(accountNumber)");
             print("Initial Balance:\(initialBalance)")
+            overdraftStatus = false;
         }
+        
+        return overdraftStatus
         
     }
     
+    func checkOverDraft()
+    {
+        if(overdraftProtector == true)
+        {
+            if(balance < -500 )
+            {
+                print("Deposit more money. You aren't allowed to overdraft more than 500")
+            }
+                
+            else
+            {
+                balance = balance - 0.50
+                print("Balance after overdraft")
+                totalBalance();
+            }
+        }
+            
+        else {
+            print("You do not have overdraft protection!")
+        }
+    }
+    //    func overdraftCheck()
+    //    {
+    //
+    //        var overDraft = createAccount(accountType: caseAccount)
+    //
+    //        if(overDraft == true)
+    //        {
+    //            if(balance < -500 )
+    //            {
+    //                print("Deposit more money. You aren't allowed to overdraft more than 500")
+    //            }
+    //
+    //            else
+    //            {
+    //                balance = balance - 0.50
+    //                print("Balance after overdraft")
+    //                totalBalance();
+    //            }
+    //        }
+    //
+    //        else {
+    //            print("You do not have overdraft protection!")
+    //        }
+    //    }
     
     func deposit(amountToDeposit: Double)
     {
@@ -152,6 +222,10 @@ class Account: Bank
         print("Total balance is \(balance)");
     }
     
+    
+    //    func changePin()
+    
+    
 }
 
 
@@ -159,17 +233,36 @@ class Account: Bank
 var bank = Bank();
 bank.welcome(nameOfBank: "RBC");
 
-var client = Client(name: "Ekta", address: "Woodbridge", age: 23, dob: "09-10-1995", gender: "Female",nameOfBank: "RBC");
+var accountInstance = Account();
+var caseAccount = Account.accountTypes.Chequing
+//accountInstance.createAccount(accountType: caseAccount)
+//accountInstance.deposit(amountToDeposit: 2500)
+//accountInstance.withdrawal(amountToWithdraw: 1000)
+
+var client = Client(name: "Ekta", address: "Woodbridge", age: 23, dob: "09-10-1995", gender: "Female",nameOfBank: "RBC", account: accountInstance);
 print("Name: \(client.name), Address: \(client.address), Age:  \(client.age), Date of birth: \(client.dob), Gender: \(client.gender), Bank name: \(client.nameOfBank)");
 
 client.welcome(nameOfBank: "Scotiabank");
-print("Want to change your name and address?")
-client.changeProfile(newName: "Kavita", newAddress: "Vaughan");
+print("Want to change your name?")
+client.changeName(newName: "Kavita");
+print("Want to change your address?")
+client.changeAddress(newAddress: "Vaughan");
+print("Want to change your pin?")
+client.changePin(newPin: 1945)
+
+print("What type of account you want to create?")
+var clientAccount = accountInstance;
+clientAccount.createAccount(accountType: caseAccount)
+clientAccount.deposit(amountToDeposit: 2500)
+bank.giveTransactionHistory()
+clientAccount.withdrawal(amountToWithdraw: 1000)
+bank.giveTransactionHistory()
+clientAccount.checkOverDraft()
 
 
-var accountInstance = Account();
-var caseAccount = Account.accountTypes.Chequing
-accountInstance.createAccount(accountType: caseAccount)
-accountInstance.deposit(amountToDeposit: 2500)
-accountInstance.withdrawal(amountToWithdraw: 1000)
+
+
+
+
+
 
